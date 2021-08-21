9f38357e04f1dd80ec8307a27da73bd20881989c
muhittinpalamutcu <muhittinpalamutcu@gmail.com>
2021-08-21 21:29:45 +0300

    Project structure

---
+++ src/main/java/io/github/muhittinpalamutcu/secondhomeworkmuhittinpalamutcu/SecondHomeworkMuhittinpalamutcuApplication.java
@@ -0,0 +1,13 @@
+package io.github.muhittinpalamutcu.secondhomeworkmuhittinpalamutcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHomeworkMuhittinpalamutcuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHomeworkMuhittinpalamutcuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/muhittinpalamutcu/secondhomeworkmuhittinpalamutcu/SecondHomeworkMuhittinpalamutcuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.muhittinpalamutcu.secondhomeworkmuhittinpalamutcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHomeworkMuhittinpalamutcuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
